3fdde023e6d3542b1ef800386e1f7b47536d426e
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-05 23:21:47 -0700

    form complete, minus onSubmit functionality

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -12,17 +12,24 @@ const Address = styled.div`
 `;
 
 function ContactForm(props) {
-
-  const {inputs, handleChange, handleSubmit} = useContactForm();
+  const doAThing = () => {              // this is where I put the functionality of the form submit
+    alert(`Message Sent
+          Name: ${inputs.firstName} ${inputs.lastName}
+          Email: ${inputs.Email}
+          Phone: ${inputs.phone}
+          Subject: ${inputs.subject}
+          Message: ${inputs.message}`)
+  }
+  const {inputs, handleChange, handleSubmit} = useContactForm(doAThing);
 
   return (
     <Form onSubmit={handleSubmit}>
-      <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} required />
-      <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} />
-      <input type="email" name="email" placeholder="Email Address" onChange={handleChange} required />
-      <input type="phone" name="phone" placeholder="555-555-5555" onChange={handleChange} />
-      <input type="text" name="subject" placeholder="Subject" onChange={handleChange} required />
-      <textarea type="text" name="message" placeholder="Type your message here..." onChange={handleChange} required />
+      <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} value={inputs.firstName} required />
+      <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} value={inputs.lastName} />
+      <input type="email" name="email" placeholder="Email Address" onChange={handleChange} value={inputs.email} required />
+      <input type="phone" name="phone" placeholder="555-555-5555" onChange={handleChange} value={inputs.phone} />
+      <input type="text" name="subject" placeholder="Subject" onChange={handleChange} value={inputs.subject} required />
+      <textarea type="text" name="message" placeholder="Type your message here..." onChange={handleChange} value={inputs.message} required />
       <button type="submit">Submit</button>
     </Form>
   );
