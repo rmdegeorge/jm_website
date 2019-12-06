b41c33cb7584a931898dfc0656e4ee29153a25b9
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-06 10:14:22 -0700

    fixed hover state so that there wasn't a shift from adding the bottome border

--- frontend/src/components/NavLink.js
+++ frontend/src/components/NavLink.js
@@ -5,11 +5,11 @@ export const NavLink = styled(Link)`
   color: #000000;
   text-decoration: none;
   margin: 5px;
-  box-sizing: initial;
-
+  box-sizing: content-box;
 
   :hover {
     border-bottom: 1px solid #000000;
+    border-top: 1px solid rgba(0,0,0,0)
   }
 
 `;
