5f24dfa089db0c626623acd8ab33c0bc26d2830b
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-09 14:04:59 -0700

    changed Header logo and worked on theming of navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,26 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import {NavLink} from './NavLink';
 
+import WordmarkClay from '../assets/JM_wordmark_clay.png';
+
 const NavbarContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 150px;
+  height: auto;
   position: relative;
+  background-color: ${props => props.theme.primary2};
   top: 0;
   left: 0;
   right: 0;
 `;
-const Logo = styled.div`
+const Header = styled.div`
   width: 90%;
   border-bottom: 1px solid grey;
   text-align: center;
-  > img {
-    width: 100px;
-    height: auto;
-  }
+`;
+const HeaderLogo = styled.img`
+  object-fit: cover;
+  object-position: 0 -50px;
+  width: 300px;
+  height: 200px;
 `;
 const LinkWrapper = styled.div`
   display: flex;
@@ -35,17 +40,18 @@ const LinkWrapper = styled.div`
 const Navbar = (props) => {
   return (
     <NavbarContainer>
-      <Logo>
-        <img src="../assets/JM_wordmark_blk.png" alt="Jasper Moon Wellness" />
-      </Logo>
+      <Header>
+        <NavLink to="/">
+          <HeaderLogo src={WordmarkClay} alt="Jasper Moon Wellness" />
+        </NavLink>
+      </Header>
       <LinkWrapper>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/About">About</NavLink>
         <NavLink to="/Classes">Classes</NavLink>
         <NavLink to="/CorporateWellness">Corporate Wellness</NavLink>
-        <NavLink to="/About">About</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
         <NavLink to="/Blog">Blog</NavLink>
         <NavLink to="/Store">Store</NavLink>
+        <NavLink to="/Contact">Contact</NavLink>
       </LinkWrapper>
     </NavbarContainer>
   )
--- frontend/src/theme/GlobalStyle.js
+++ frontend/src/theme/GlobalStyle.js
@@ -1,23 +1,47 @@
 import {createGlobalStyle} from 'styled-components';
 
-
-export default const GlobalStyle = createGlobalStyle`
-
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  * {
-    box-sizing: border-box;
-  }
-`;
+export const palette = {
+  primary1: {
+    two: '#A9C1B8',
+    three: '#7EA295',
+    four: '#538372',
+    five: '#2b7561',
+    six: '#20503F',
+    seven: '#183C2F',
+    eight: '#10281F'
+  },
+  primary2: {
+    two: '#DEEDE5',
+    three: '#CEE4D9',
+    four: '#BEDBCC',
+    five: '#b8d9cc',
+    six: '#8BA899',
+    seven: '#687E73',
+    eight: '#45544C'
+  },
+  secondary1: {
+    two: '#D9B5BB',
+    three: '#C6909A',
+    four: '#B36B78',
+    five: '#b55c6b',
+    six: '#803845',
+    seven: '#602A34',
+    eight: '#401C22',
+  },
+  secondary2: {
+    two: '#F2DEE3',
+    three: '#ECCED5',
+    four: '#E6BEC7',
+    five: '#e8bdc4',
+    six: '#B38B94',
+    seven: '#86686F',
+    eight: '#59454A'
+  },
+  functional1: '#fafafa',
+  functional2: '#c7c7c7',
+  functional3: '#7A7979',
+  functional4: '#5c5c5c'
+}
 
 const size = {
   mobile: '425px',
@@ -29,53 +53,33 @@ export const device = {
   tablet: `(min-width: ${size.tablet})`,
   computer: `(min-width: ${size.computer})`,
 };
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    margin: 0;
+    padding: 0;
+    background-color: ${palette.functional1}
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
 export const theme = {
-  primary1: {
-    200: '#A9C1B8',
-    300: '#7EA295',
-    400: '#538372',
-    500: '#2b7561',
-    600: '#20503F',
-    700: '#183C2F',
-    800: '#10281F'
-  },
-  primary2: {
-    200: '#DEEDE5',
-    300: '#CEE4D9',
-    400: '#BEDBCC',
-    500: '#b8d9cc',
-    600: '#8BA899',
-    700: '#687E73',
-    800: '#45544C'
-  },
-  secondary1: {
-    200: '#D9B5BB',
-    300: '#C6909A',
-    400: '#B36B78',
-    500: '#b55c6b',
-    600: '#803845',
-    700: '#602A34',
-    800: '#401C22',
-  },
-  secondary2: {
-    200: '#F2DEE3',
-    300: '#ECCED5',
-    400: '#E6BEC7',
-    500: '#e8bdc4',
-    600: '#B38B94',
-    700: '#86686F',
-    800: '#59454A'
-  },
-  functional1: {
-    '#fafafa'
-  },
-  functional2: {
-    '#c7c7c7'
-  },
-  functional3: {
-    '#7A7979'
-  },
-  functional4: {
-    '#5c5c5c'
-  },
+  primary1: palette.primary1.five,
+  primary2: palette.primary2.five,
+  secondary1: palette.secondary1.five,
+  secondary2: palette.secondary2.five,
+  functional1: palette.functional1,
+  functional2: palette.functional1,
+  functional3: palette.functional1,
+  functional4: palette.functional1
 }
+
+export default GlobalStyle;
