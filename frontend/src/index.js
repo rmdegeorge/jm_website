8c9cc4cc76b952ba140e33e86130ff97f61cee0d
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-11 11:39:56 -0700

    wrapped site in  context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-
+import DataContextProvider from './context/DataContextProvider';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root')
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
