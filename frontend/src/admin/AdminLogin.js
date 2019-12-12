4e645a73f176d28dc60ad30df0325bcd373a2ba5
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-12 10:11:03 -0700

    made this page show a welcome message instead of login form when logged in.

--- frontend/src/admin/AdminLogin.js
+++ frontend/src/admin/AdminLogin.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components';
 
+import {DataContext} from '../context/DataContextProvider';
 import AdminLoginForm from './AdminLoginForm';
 
-const AdminLoginWrapper = styled.div``;
+const AdminLoginWrapper = styled.div`
+
+`;
+const WelcomeMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100 0 400px 0;
+`;
+
 
 function AdminLogin(props) {
+  const {token,user} = useContext(DataContext);
+  console.log(user)
   return(
     <AdminLoginWrapper>
-      Admin Login Page
-      <AdminLoginForm />
+      {
+        token
+        ?
+        <WelcomeMessage>
+          <h2>Welcome!</h2>
+          <p>You are currently logged in as {user.username}</p>
+        </WelcomeMessage>
+        :
+        <AdminLoginForm />
+      }
+
     </AdminLoginWrapper>
   );
 };
