23528caad55b2fd13af6345085b4cc2bb10c54c0
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-18 16:06:59 -0700

    updated breakpoints for media queries

--- client/src/theme/GlobalStyle.js
+++ client/src/theme/GlobalStyle.js
@@ -1,64 +1,66 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export const palette = {
   primary1: {
-    two: '#A9C1B8',
-    three: '#7EA295',
-    four: '#538372',
-    five: '#2b7561',
-    six: '#20503F',
-    seven: '#183C2F',
-    eight: '#10281F'
+    two: "#A9C1B8",
+    three: "#7EA295",
+    four: "#538372",
+    five: "#2b7561",
+    six: "#20503F",
+    seven: "#183C2F",
+    eight: "#10281F"
   },
   primary2: {
-    two: '#DEEDE5',
-    three: '#CEE4D9',
-    four: '#BEDBCC',
-    five: '#b8d9cc',
-    six: '#8BA899',
-    seven: '#687E73',
-    eight: '#45544C'
+    two: "#DEEDE5",
+    three: "#CEE4D9",
+    four: "#BEDBCC",
+    five: "#b8d9cc",
+    six: "#8BA899",
+    seven: "#687E73",
+    eight: "#45544C"
   },
   secondary1: {
-    two: '#D9B5BB',
-    three: '#C6909A',
-    four: '#B36B78',
-    five: '#b55c6b',
-    six: '#803845',
-    seven: '#602A34',
-    eight: '#401C22',
+    two: "#D9B5BB",
+    three: "#C6909A",
+    four: "#B36B78",
+    five: "#b55c6b",
+    six: "#803845",
+    seven: "#602A34",
+    eight: "#401C22"
   },
   secondary2: {
-    two: '#F2DEE3',
-    three: '#ECCED5',
-    four: '#E6BEC7',
-    five: '#e8bdc4',
-    six: '#B38B94',
-    seven: '#86686F',
-    eight: '#59454A'
+    two: "#F2DEE3",
+    three: "#ECCED5",
+    four: "#E6BEC7",
+    five: "#e8bdc4",
+    six: "#B38B94",
+    seven: "#86686F",
+    eight: "#59454A"
   },
-  functional1: '#fafafa',
-  functional2: '#c7c7c7',
-  functional3: '#7A7979',
-  functional4: '#5c5c5c'
-}
+  functional1: "#fafafa",
+  functional2: "#c7c7c7",
+  functional3: "#7A7979",
+  functional4: "#5c5c5c"
+};
 
-const size = {
-  mobile: '425px',
-  tablet: '768px',
-  computer: '1024px',
+const breakpoint = {
+  mobile: "599px",
+  tabletPortrait: "900px",
+  tabletLandscape: "1200px",
+  desktop: "1800px"
 };
 export const device = {
-  mobile: `(min-width: ${size.mobile})`,
-  tablet: `(min-width: ${size.tablet})`,
-  computer: `(min-width: ${size.computer})`,
+  mobile: `(max-width: ${breakpoint.mobile})`,
+  tabletPortrait: `(min-width: ${breakpoint.mobile})`,
+  tabletLandscape: `(min-width: ${breakpoint.tabletPortrait})`,
+  desktop: `(min-width: ${breakpoint.tabletLandscape})`,
+  desktopLarge: `(min-width: ${breakpoint.desktop})`
 };
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap');
   body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
+    font-family: "Source Sans Pro", "Open Sans", -apple-system, BlinkMacSystemFont, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 
@@ -80,6 +82,6 @@ export const theme = {
   functional2: palette.functional1,
   functional3: palette.functional1,
   functional4: palette.functional1
-}
+};
 
 export default GlobalStyle;
