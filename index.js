83f9d5b40fb89e31888293c2d5010c0a8f0417ca
Rob DeGeorge <rmdegeorge@gmail.com>
2020-12-03 07:47:10 -0700

    updated MONGODB_URI

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const expressJwt = require("express-jwt");
-
+const MongoClient = require("mongodb").MongoClient;
 const fileUpload = require("express-fileupload");
+const querystring = require("querystring");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+
+//********************************************
+//***** Connecting to mongodb atlas here *****
+//********************************************
+//
+//const uri = "mongodb+srv://" + querystring.escape(process.env.MONGODB_USERNAME) + ":" + querystring.escape(process.env.MONGODB_PASSWORD) + "@testcluster1.ptja5.mongodb.net/<dbname>?retryWrites=true&w=majority"
+//MongoClient.connect(uri, function(err, client){
+//	if(err) {
+//		console.log('An error occurred while connecting to MongoDB Atlass...\n', err);
+//	}
+//	console.log('Connected to MongoDB...');
+//	const collection = client.db("test").collection("devices");
+//	// perform actions on the collection object
+//	client.close();
+//});
+//
+//********************************************
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(fileUpload());
 
 // send email from contact form
-app.use("/send", require("./routes/contactRouter"));
+//app.use("/send", require("./routes/contactRouter"));
 
 // upload files
 app.use("/upload", require("./routes/fileUploadRouter"));
